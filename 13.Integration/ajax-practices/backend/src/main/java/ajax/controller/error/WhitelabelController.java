28168db661e115afe5121991ac331135b577c0c0
eu2525 <eu2525@khu.ac.kr>
2025-02-18 17:31:42 +0900

    ajax study

--- 13.Integration/ajax-practices/backend/src/main/java/ajax/controller/error/WhitelabelController.java
+++ 13.Integration/ajax-practices/backend/src/main/java/ajax/controller/error/WhitelabelController.java
@@ -3,6 +3,7 @@ package ajax.controller.error;
 import java.util.Optional;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,7 +33,7 @@ public class WhitelabelController implements ErrorController {
 	@RequestMapping("/500")
 	public ResponseEntity<JsonResult<String>> _500(@RequestAttribute String errors) {
 		return ResponseEntity
-				.internalServerError()
+				.status(HttpStatus.OK)
 				.body(JsonResult.fail(errors));
 	}
 
--- 13.Integration/ajax-practices/frontend/src/App.js
+++ 13.Integration/ajax-practices/frontend/src/App.js
@@ -3,9 +3,11 @@ import React, {useEffect, useState, useRef} from 'react';
 import Modal from "react-modal";
 import ReactModal from "react-modal";
 import styled from 'styled-components';
+import serialize from 'form-serialize'
+import axios from 'axios'
+
 import './assets/scss/App.scss';
 import * as stylesModal from './assets/scss/Modal.scss';
-import data from './assets/json/data.js';
 
 const CreateForm = styled.form``;
 const UpdateForm = styled.form``;
@@ -16,16 +18,131 @@ const Item = styled.li``;
 ReactModal.setAppElement("body");
 
 function App() {
-    const [items, setItems] = useState(data);
-    const [modalOpen, setModalOpen] = useState(false);
+    const refCreateForm = useRef(null);
+    const [items, setItems] = useState(null);
+    
+    const [modalData, setModalData] = useState({
+        open: false,
+        itemType: '',
+        itemName: ''
+    })
 
+    const addItem = async (item) => {
+        try {
+            const response = await fetch('/item', {
+                method: "post",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(item)
+            });
+
+            const jsonResult = await response.json();
+
+            if(!response.ok || jsonResult.result === 'fail') {
+                throw new Error(`${response.status} ${jsonResult.message}`);
+            }
+
+            setItems([jsonResult.data, ...items]);
+            refCreateForm.current.reset();
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    const fetchItems = async () => {
+        try {
+            const response = await fetch('/item', {
+                method: "get",
+                headers: {
+                    'Accept': 'application/json'
+                },
+                body: null
+            });
+
+            const jsonResult = await response.json();
+
+            if(!response.ok || jsonResult.result === 'fail') {
+                throw new Error(`${response.status} ${jsonResult.message}`);
+            }
+
+            setItems(jsonResult.data);
+        } catch(err) {
+            console.error(err);
+        }
+    };
+
+    const clickItemName = async (id) => {
+        try {
+            const response = await axios.get(`/item/${id}`)
+            const jsonResult = response.data;
+
+            setModalData(update(modalData, {
+                open: {$set: true},
+                itemType: {$set: jsonResult.data.type},
+                itemName: {$set: jsonResult.data.name}
+            }));
+        } catch(err) {
+            console.error(err.response ? `${err.response.status} ${err.response.data.message}` : err);
+        }        
+    }
+
+    const deleteItem = async (id) => {
+        try {
+            const response = await axios.delete(`/item/${id}`)
+            const jsonResult = response.data;
+            
+            setItems(items.filter((e) => e.id != jsonResult.data));
+        } catch(err) {
+            console.error(err.response ? `${err.response.status} ${err.response.data.message}` : err);
+        }
+    }
+
+    useEffect(() => {
+        fetchItems();
+    }, []);
     return (
         <div id='App'>
-
             <h1>AJAX: Restful API</h1>
 
             <div>
-                <CreateForm>
+                <form
+                    ref={refCreateForm}
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        
+                        try {
+                            /*
+                            const item = Array.from(e.target, (el) => {
+                                if(el.name !== '' && el.value === '') {
+                                    throw new Error(`validation ${el.name} is empty`);
+                                }
+
+                                return {name: el.name, value: el.value};
+                            })
+                            .filter(({name}) => name !== '')
+                            .reduce((res, {name, value}) => {
+                                res[name] = value;
+                                return res;
+                            }, {});
+                            */
+
+                            Array.from(e.target, (el) => {
+                                if(el.name !== '' && el.value === '') {
+                                    throw new Error(`validation ${el.name} is empty`);
+                                }
+                                return null;
+                            })
+
+                            // const queryString = serialize(e.target);
+                            const item = serialize(e.target, {hash: true});
+
+                            addItem(item); 
+                        } catch(err) {
+                            console.error(err);
+                        }
+                    }}>
                     <select name={'type'}>
                         <option>BOOK</option>
                         <option>CLOTHE</option>
@@ -38,8 +155,10 @@ function App() {
                     {' '}
                     <input type={'text'} name={'name'} placeholder={'name'}/>
                     <input type={'submit'} value={'[C]reate (post)'}/>
-                </CreateForm>
-                <form>
+                </form>
+
+
+                <CreateForm>
                     <select name={'type'}>
                         <option>BOOK</option>
                         <option>CLOTHE</option>
@@ -53,21 +172,25 @@ function App() {
                     <input type={'text'} name={'name'} placeholder={'name'}/>
                     <input type={'file'} name={'file'} />
                     <input type={'submit'} value={'[C]reate (post)'}/>
-                </form>
+                </CreateForm>
             </div>
 
 
-            <h2 title={'[R]ead (get)'}>Items</h2>
+            <h2
+                title={'[R]ead (get)'}
+                onClick={() => fetchItems()}>Items</h2>
 
 
             <ItemList>
                 {
                     items?.map((item, index) => <Item key={item.id}>
                         <h4>
-                            <b 
+                            <b
                                 title={'[R]ead (get)'}
-                                onClick={() => setModalOpen(!modalOpen)}>{item.name}</b>
-                            <button>{'[D]elete (delete)'}</button>
+                                onClick={() => clickItemName(item.id)}>{item.name}</b>
+                            <button onClick={() => {
+                                deleteItem(item.id);
+                            }}>{'[D]elete (delete)'}</button>
                         </h4>
                         <div>
                             <span>
@@ -83,16 +206,27 @@ function App() {
 
 
             <Modal
-                isOpen={modalOpen}
-                onRequestClose={() => setModalData(!modalOpen)}
+                isOpen={modalData.open}
+                onRequestClose={() => setModalData(update(modalData, {open: {$set: false}}))}
                 className={stylesModal.Modal}
                 overlayClassName={stylesModal.Overlay}
                 style={{content: {width: 280}}}>
                 <h3>Update Item</h3>
-                <UpdateForm>
+                <form onChange={(e) => {
+                    console.log('tomorrow~~');
+                }}>
                     <label>TYPE</label>
                     {' '}
-                    <select name={'type'}>
+                    <select
+                        name={'type'}
+                        value={modalData.itemType}
+                        onChange={(e) => {
+                            setModalData(update(modalData, {
+                                itemType: {
+                                    $set: e.target.value
+                                }
+                            }));
+                        }}>
                         <option>BOOK</option>
                         <option>CLOTHE</option>
                         <option>MUSIC</option>
@@ -104,10 +238,20 @@ function App() {
                     <br/><br/>
                     <label>NAME</label>
                     {' '}
-                    <input type={'text'} name={'name'} />   
+                    <input
+                        type={'text'}
+                        name={'name'}
+                        value={modalData.itemName}
+                        onChange={(e) => {
+                            setModalData(update(modalData, {
+                                itemName: {
+                                    $set: e.target.value
+                                }
+                            }));  
+                        }}/>   
                     <hr />
                     <input type={"submit"} value={'[U]pdate (update)'} />
-                </UpdateForm>
+                </form>
             </Modal>
 
         </div>
